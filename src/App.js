712405e6a88d276ff9ddb82dd640b10c785b3663
Juanci <juancibanez2003@gmail.com>
2025-03-14 00:23:40 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
